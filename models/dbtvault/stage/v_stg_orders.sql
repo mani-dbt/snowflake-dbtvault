7b482a9e472830ae463731b12c24f27c9d9fb8a1
Mani Pachineelam <mani.pachineelam@dbtlabs.com>
2024-02-21 01:51:51 +0000

    change to automate_dv package

--- models/dbtvault/stage/v_stg_orders.sql
+++ models/dbtvault/stage/v_stg_orders.sql
@@ -98,7 +98,7 @@ hashed_columns:
 {% set hashed_columns = metadata_dict['hashed_columns'] %}
 
 WITH staging AS (
-{{ dbtvault.stage(include_source_columns=true,
+{{ automate_dv.stage(include_source_columns=true,
                   source_model=source_model,
                   derived_columns=derived_columns,
                   hashed_columns=hashed_columns,
